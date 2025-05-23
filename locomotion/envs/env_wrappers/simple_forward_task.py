92ded3211e5f11feaeef24157124f140ad30a03e
GARANCINI <clement.garancini@orange.fr>
2025-05-23 13:40:41 -0400

    Sensor update, dependencies fix and training script

--- locomotion/envs/env_wrappers/simple_forward_task.py
+++ locomotion/envs/env_wrappers/simple_forward_task.py
@@ -22,38 +22,43 @@ import numpy as np
 
 
 class SimpleForwardTask(object):
-  """Default empy task."""
-  def __init__(self):
-    """Initializes the task."""
-    self.current_base_pos = np.zeros(3)
-    self.last_base_pos = np.zeros(3)
+    """Default empy task."""
 
-  def __call__(self, env):
-    return self.reward(env)
+    def __init__(self, target_vel: float):
+        """Initializes the task."""
+        self.target_vel = target_vel
+        self.sigma = 0.33
+        self.current_vel = 0
+        self.current_base_pos = np.zeros(3)
+        self.last_base_pos = np.zeros(3)
 
-  def reset(self, env):
-    """Resets the internal state of the task."""
-    self._env = env
-    self.last_base_pos = env.robot.GetBasePosition()
-    self.current_base_pos = self.last_base_pos
+    def __call__(self, env):
+        return self.reward(env)
 
-  def update(self, env):
-    """Updates the internal state of the task."""
-    self.last_base_pos = self.current_base_pos
-    self.current_base_pos = env.robot.GetBasePosition()
+    def reset(self, env):
+        """Resets the internal state of the task."""
+        self._env = env
+        self.last_base_pos = env.robot.GetBasePosition()
+        self.current_base_pos = self.last_base_pos
+        self.current_vel = 0
 
-  def done(self, env):
-    """Checks if the episode is over.
+    def update(self, env):
+        """Updates the internal state of the task."""
+        self.last_base_pos = self.current_base_pos
+        self.current_base_pos = env.robot.GetBasePosition()
+        self.current_vel = self.current_base_pos[0] - self.last_base_pos[0]
 
-       If the robot base becomes unstable (based on orientation), the episode
-       terminates early.
-    """
-    rot_quat = env.robot.GetBaseOrientation()
-    rot_mat = env.pybullet_client.getMatrixFromQuaternion(rot_quat)
-    return rot_mat[-1] < 0.85
+    def done(self, env):
+        """Checks if the episode is over.
 
+        If the robot base becomes unstable (based on orientation), the episode
+        terminates early.
+        """
+        rot_quat = env.robot.GetBaseOrientation()
+        rot_mat = env.pybullet_client.getMatrixFromQuaternion(rot_quat)
+        return rot_mat[-1] < 0.85
 
-  def reward(self, env):
-    """Get the reward without side effects."""
-    del env
-    return self.current_base_pos[0] - self.last_base_pos[0]
+    def reward(self, env):
+        """Get the reward without side effects."""
+        del env
+        return np.exp(-((self.current_vel - self.target_vel) ** 2) / self.sigma)
---
+++ training.py
@@ -0,0 +1,75 @@
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.callbacks import CheckpointCallback
+from stable_baselines3.common.monitor import Monitor
+import argparse
+from datetime import datetime
+import os
+import json
+import shutil
+import gym
+
+
+def load_config(json_path):
+    with open(json_path, "r") as f:
+        return json.load(f)
+
+
+def main(config_path: str):
+    config = load_config(config_path)
+    # num_envs = config["environment"]["num_envs"]
+    total_timesteps = config["training"]["total_timesteps"]
+    checkpoint_freq = config["training"]["checkpoint_timesteps"]
+    checkpoint_freq = checkpoint_freq
+
+    task = "locomotion:A1GymEnv-v1"
+
+    timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    logfile = "PPO_" + task + "_" + str(timestamp)
+    logfile_monitor = os.path.join(
+        "/Users/clement/Documents/PolyMontreal/locomotion_simulation/logs/monitor",
+        logfile,
+        "",
+    )
+    logfile_model = os.path.join(
+        "/Users/clement/Documents/PolyMontreal/locomotion_simulation/logs/models",
+        logfile,
+    )
+    logfile_parameters = os.path.join(
+        "/Users/clement/Documents/PolyMontreal/locomotion_simulation/logs/parameters",
+        logfile + ".json",
+    )
+    logfile_checkpoints = (
+        "/Users/clement/Documents/PolyMontreal/locomotion_simulation/logs/checkpoints"
+    )
+
+    print("Making gym envs...")
+    env = gym.make(task)
+    env = Monitor(env, filename=logfile_monitor)
+    print("Done")
+
+    model = PPO("MlpPolicy", env=env, verbose=1, n_steps=512)
+
+    shutil.copy(config_path, logfile_parameters)
+    print("Experiments parameters saved at: ", logfile_parameters)
+
+    cp_callback = CheckpointCallback(
+        save_freq=checkpoint_freq,
+        save_path=logfile_checkpoints,
+        name_prefix=logfile,
+        verbose=2,
+    )
+
+    print("Starting training...")
+    model.learn(total_timesteps=total_timesteps, log_interval=1, callback=cp_callback)
+    model.save(logfile_model)
+    print(f"Saved model at: {logfile_model}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", type=str, help="Path to JSON config file")
+    args = parser.parse_args()
+
+    config_path = args.config
+    main(config_path)
